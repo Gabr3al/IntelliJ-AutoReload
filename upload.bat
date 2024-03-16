539c22091e5598d73bd9c3a332253be2a302aeb4
Gabr3al <83646288+Gabr3al@users.noreply.github.com>
2024-03-16 20:27:16 +0100

    Add files via upload

--- upload.bat
+++ upload.bat
@@ -1,13 +1,13 @@
 @echo off
 
 del uploadscript.txt
-echo open sftp://USERNAME@HOSTNAME -privatekey="C:PATH\TO\PRIVATEKEY" >> uploadscript.txt
-echo cd /PATH/TO/PLUGIN/FOLDER >> uploadscript.txt
-echo put C:\PATH\TO\COMPILED\FILE.jar >> uploadscript.txt
+echo open sftp://USERNAME@HOSTNAME -privatekey="C:\PATH\TO\PRIVATEKEY.ppk" >> uploadscript.txt
+echo cd /HOME/PATH/TO/SERVER/plugins >> uploadscript.txt
+echo put C:\PATH\TO\YOUR\PROJECT\target\YOURPLUGINNAME.jar >> uploadscript.txt
 echo close >> uploadscript.txt
 echo exit >> uploadscript.txt
 
 "C:\Program Files (x86)\WinSCP\WinSCP.com" "/script=uploadscript.txt"
 del uploadscript.txt
 
-curl "http://HOSTNAME:CONFIG-PORT/?auth=YOURAUTHKEY"
+curl "http://HOSTNAME:STOPAPI-CONFIG-PORT/?auth=AUTHKEY"
